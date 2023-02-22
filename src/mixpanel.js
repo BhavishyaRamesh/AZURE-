2d468e3be5ad010b10968acb0aa4dc1d5f6abf87
BhavishyaRamesh <101257695+BhavishyaRamesh@users.noreply.github.com>
2023-02-22 11:59:47 +0530

    Create mixpanel.js

---
+++ src/mixpanel.js
@@ -0,0 +1,8 @@
+import mixpanel from 'mixpanel-browser';
+// or with require() syntax:
+// const mixpanel = require('mixpanel-browser');
+
+// Enabling the debug mode flag is useful during implementation,
+// but it's recommended you remove it for production
+mixpanel.init('f4dbbe710a5fc4b98189e3366b94f8e3', {debug: true}); 
+mixpanel.track('Sign up');
