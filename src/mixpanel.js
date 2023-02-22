bcce4af532228827163a4016cd0dffd68121de06
BhavishyaRamesh <101257695+BhavishyaRamesh@users.noreply.github.com>
2023-02-22 14:40:59 +0530

    Update mixpanel.js

--- src/mixpanel.js
+++ src/mixpanel.js
@@ -4,7 +4,7 @@ import mixpanel from 'mixpanel-browser';
 
 // Enabling the debug mode flag is useful during implementation,
 // but it's recommended you remove it for production
-mixpanel.init('f4dbbe710a5fc4b98189e3366b94f8e3', {debug: true}); 
+mixpanel.init('f4dbbe710a5fc4b98189e3366b94f8e3', {debug: true}, ignore_dnt:true); 
 mixpanel.track('Sign Up', {
   'source': "https://github.com/",
   'Opted out of email': true,
